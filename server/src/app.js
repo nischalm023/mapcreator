e23a49bc00edf9408f9fb2d70131d85c25cbd38b
Mahima Chaudhary <GREYORANGE\mahima.c@ggn002164.greyorange.com>
2023-09-20 17:06:55 +0530

    GM-124857 increase server size

--- server/src/app.js
+++ server/src/app.js
@@ -16,8 +16,8 @@ const FormData = require('form-data'); // added this line
 global.fetch = require("node-fetch");
 const upload = multer();
 app.use(cors());
-app.use(express.json({ limit: "50mb" }));
-app.use(express.urlencoded({ limit: "50mb" }));
+app.use(express.json({ limit: "200mb" }));
+app.use(express.urlencoded({ limit: "200mb" }));
 
 app.get("/api/test", (req, res) => res.json("test api response"));
 
